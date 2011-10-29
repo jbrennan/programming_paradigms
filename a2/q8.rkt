1215e138a786b125f59e3351b2873fe6a45cdcda
Jason Brennan <i.jasonbrennan@gmail.com>
2011-10-28 22:53:13 -0400

    The last of the code is done. Now time for testing.

--- a2/q8.rkt
+++ a2/q8.rkt
@@ -1,1 +1,11 @@
 #lang racket
+
+(define (depth lst)
+  (define (atom? x) (not (pair? x)))
+  (cond ((atom? lst) 0)
+        (else (+ (apply max (map depth lst)) 1))))
+
+(depth 'a)
+(depth '(a))
+(depth '(a (b) c))
+(depth '(((((a(((b))))))))) 
